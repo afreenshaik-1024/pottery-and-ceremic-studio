1e619ca1a859ca75e21d20bb8954c812a636ffd6
Shaik6304 <shaikafrin6304@gmail.com>
2026-05-13 13:35:23 +0530

    first commit

--- script.js
+++ script.js
@@ -2,5 +2,13 @@ const menuBtn = document.getElementById("menuBtn");
 const navLinks = document.querySelector(".nav-links");
 
 menuBtn.addEventListener("click", () => {
+
     navLinks.classList.toggle("active");
+
+    if(navLinks.classList.contains("active")){
+        menuBtn.innerHTML = "✖";
+    } else {
+        menuBtn.innerHTML = "☰";
+    }
+
 });
